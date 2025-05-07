26242ca99eb479b933efdee88eff83bdeee7bea0
Celiapalacioshumanes <cpalacios@stemdo.io>
2025-05-07 12:10:06 +0200

    Update main.tf

--- vnt_module7/modules/nsg/main.tf
+++ vnt_module7/modules/nsg/main.tf
@@ -1,1 +1,34 @@
+resource "azurerm_network_security_group" "this" {
+  name                = var.nsg.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_network_security_rule" "rules" {
+  for_each = { for rule in var.nsg.security_rules : rule.name => rule }
+
+  name                        = each.value.name
+  priority                    = each.value.priority
+  direction                   = each.value.direction
+  access                      = each.value.access
+  protocol                    = each.value.protocol
+  source_port_range           = each.value.source_port_range
+  destination_port_range      = each.value.destination_port_range
+  source_address_prefix       = each.value.source_address_prefix
+  destination_address_prefix  = each.value.destination_address_prefix
+
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.this.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "this" {
+  subnet_id                 = data.azurerm_subnet.target.id
+  network_security_group_id = azurerm_network_security_group.this.id
+}
+
+data "azurerm_subnet" "target" {
+  name                 = var.subnet_name
+  virtual_network_name = var.vnet_name
+  resource_group_name  = var.resource_group_name
+}
 
