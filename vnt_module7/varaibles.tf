849cb177b9d22afd486491ccbaedd1c92e966d56
Celiapalacioshumanes <cpalacios@stemdo.io>
2025-05-07 12:18:17 +0200

    Update varaibles.tf

--- vnt_module7/varaibles.tf
+++ vnt_module7/varaibles.tf
@@ -1,45 +1,35 @@
 variable "vnet_name" {
-  type = string
+  description = "Nombre de la VNet"
+  type        = string
 }
+
 variable "vnet_address_space" {
-  type = list(string)
+  description = "Espacio de direcciones de la VNet"
+  type        = list(string)
 }
+
 variable "location" {
-  type = string
+  description = "Ubicación del recurso"
+  type        = string
 }
+
 variable "existent_resource_group_name" {
-  type = string
+  description = "Nombre del grupo de recursos existente"
+  type        = string
 }
+
 variable "owner_tag" {
-  type = string
+  description = "Propietario de la VNet"
+  type        = string
 }
+
 variable "environment_tag" {
-  type = string
+  description = "Entorno de la VNet"
+  type        = string
 }
+
 variable "vnet_tags" {
-  type    = map(string)
-  default = {}
-}
-variable "subnets" {
-  description = "Lista de subredes con NSG opcional"
-  type = list(object({
-    name             = string
-    address_prefixes = list(string)
-    nsg = optional(object({
-      name           = string
-      security_rules = list(object({
-        name                       = string
-        priority                   = number
-        direction                  = string
-        access                     = string
-        protocol                   = string
-        source_port_range          = string
-        destination_port_range     = string
-        source_address_prefix      = string
-        destination_address_prefix = string
-      }))
-    }))
-  }))
-  default = []
+  description = "Tags adicionales para la VNet"
+  type        = map(string)
 }
 
