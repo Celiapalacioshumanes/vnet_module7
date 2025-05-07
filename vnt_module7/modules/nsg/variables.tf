c6438dd1ae4b17af899b9e30fbbfe273a1ae210e
Celiapalacioshumanes <cpalacios@stemdo.io>
2025-05-07 12:10:23 +0200

    Update variables.tf

--- vnt_module7/modules/nsg/variables.tf
+++ vnt_module7/modules/nsg/variables.tf
@@ -1,1 +1,21 @@
+variable "nsg" {
+  type = object({
+    name           = string
+    security_rules = list(object({
+      name                       = string
+      priority                   = number
+      direction                  = string
+      access                     = string
+      protocol                   = string
+      source_port_range          = string
+      destination_port_range     = string
+      source_address_prefix      = string
+      destination_address_prefix = string
+    }))
+  })
+}
+variable "subnet_name" { type = string }
+variable "resource_group_name" { type = string }
+variable "vnet_name" { type = string }
+variable "location" { type = string }
 
